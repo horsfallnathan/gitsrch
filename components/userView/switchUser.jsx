1ed7902c7960eb762a7e434cecbc91d81ffa5e82
nthn <roundingle@gmail.com>
2021-03-22 03:51:50 +0100

    feat(switchuser): implemented changing default gh user

---
+++ components/userView/switchUser.jsx
@@ -0,0 +1,36 @@
+import { forwardRef, useState } from "react";
+
+export const SwitchUser = forwardRef(({ changeUser }, ref) => {
+  const [searchString, setSearchString] = useState("");
+
+  const handleInput = (e) => {
+    const text = e.target.value.trim();
+    setSearchString(text);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    changeUser(searchString);
+  };
+  return (
+    <div ref={ref} className="userCont hasTransition switchUser">
+      <div className="userCard">
+        <p className=" mb-4">Enter their github username</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="enter username"
+            onChange={(e) => handleInput(e)}
+          />
+        </form>
+        <button
+          type="submit"
+          className="btn-sm btn-pri mt-4"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Change User
+        </button>
+      </div>
+    </div>
+  );
+});
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,52 @@
 import { UserView } from "../components/userView/index";
 import { RepoList } from "../components/repo/index";
 import { Layout } from "../components/layout";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { SwitchUser } from "../components/userView/switchUser";
 
 export default function Index() {
-  const userRef = useRef();
+  const [user, setUser] = useState("horsfallnathan");
+  const [userMode, setUserMode] = useState(true);
+  const value = { user, setUser };
+
+  const switchCardRef = useRef();
+  const userCardRef = useRef();
+
+  const handleSwitch = (e) => {
+    // normally i'd use cssTransition group + css animations or gsap but...
+    if (userMode) {
+      setTimeout(() => {
+        userCardRef.current.style.display = "none";
+        switchCardRef.current.style.display = "block";
+      }, 400);
+      setUserMode(false);
+    } else {
+      switchCardRef;
+      setTimeout(() => {
+        switchCardRef.current.style.display = "none";
+        userCardRef.current.style.display = "block";
+      }, 400);
+      setUserMode(true);
+    }
+  };
   return (
     <Layout>
       <section className="headerSection"></section>
       <section className="pageContainer mt-24">
         <div className="mainGrid">
-          <UserView ref={userRef} />
+          <div className="stickyCont">
+            <div className="sticky">
+              <SwitchUser ref={switchCardRef} changeUser={setUser} />
+              <UserView ref={userCardRef} />
+              <button
+                className="mt-4 btn-sm btn-pri"
+                onClick={(e) => handleSwitch(e)}
+              >
+                switch user
+              </button>
+            </div>
+          </div>
+
           <RepoList />
         </div>
       </section>
